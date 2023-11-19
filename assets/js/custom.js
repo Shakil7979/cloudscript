999c051799b0cbfc6b82e57d00cb739495b5707e
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2023-11-20 00:42:59 +0600

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,6 +15,34 @@ $(document).ready(function(){
 		$('.child_class_item_4').removeClass('activelink');
 		$(this).addClass('activelink'); 
 	});
+	$('.child_class_item_5').click(function(){
+		$('.child_class_item_5').removeClass('activelink');
+		$(this).addClass('activelink'); 
+	});
+	$('.child_class_item_6').click(function(){
+		$('.child_class_item_6').removeClass('activelink');
+		$(this).addClass('activelink'); 
+	});
+	$('.child_class_item_7').click(function(){
+		$('.child_class_item_7').removeClass('activelink');
+		$(this).addClass('activelink'); 
+	});
+	$('.child_class_item_8').click(function(){
+		$('.child_class_item_8').removeClass('activelink');
+		$(this).addClass('activelink'); 
+	});
+	$('.child_class_item_9').click(function(){
+		$('.child_class_item_9').removeClass('activelink');
+		$(this).addClass('activelink'); 
+	});
+	$('.child_class_item_10').click(function(){
+		$('.child_class_item_10').removeClass('activelink');
+		$(this).addClass('activelink'); 
+	});
+	$('.child_class_item_11').click(function(){
+		$('.child_class_item_11').removeClass('activelink');
+		$(this).addClass('activelink'); 
+	});
 	$('.unkown_age_sk').click(function(){
 		$('.unkown_age_sk').removeClass('activelink');
 		$(this).addClass('activelink'); 
